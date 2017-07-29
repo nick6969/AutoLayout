08f80581af59c7faa0a3b9d1f83f204a6150f911
Nick Lin <hawhaw.ya@gmail.com>
2017-07-29 09:45:01 +0800

    添加新的寫法，可以減少 code 的數量

--- AutoLayout.swift
+++ AutoLayout.swift
@@ -8,41 +8,90 @@
 
 import UIKit
 
-// 自己建立的簡單的使用 AutoLayout
-extension UIView{
-    
-    func mLay(_ attribute:NSLayoutAttribute,_ relatedBy:NSLayoutRelation,_ toItem:Any?,_ attribute1:NSLayoutAttribute , multiplier: CGFloat , constant: CGFloat)->NSLayoutConstraint{
-        self.translatesAutoresizingMaskIntoConstraints = false
-        return NSLayoutConstraint(item: self, attribute: attribute, relatedBy: relatedBy, toItem: toItem, attribute: attribute1, multiplier: multiplier, constant: constant)
+public struct OptionalEdge {
+    public var top: CGFloat?
+    public var left: CGFloat?
+    public var bottom: CGFloat?
+    public var right: CGFloat?
+    public init(top: CGFloat? = nil, left: CGFloat? = nil, bottom: CGFloat? = nil, right: CGFloat? = nil){
+        self.top = top
+        self.left = left
+        self.bottom = bottom
+        self.right = right
     }
-    
-    func mLay(_ attribute:NSLayoutAttribute,_ relatedBy:NSLayoutRelation,_ toItem:Any?)->NSLayoutConstraint{
+}
+
+// MARK:- extension UIView - With AutoLayout
+extension UIView {
+
+    @discardableResult func mLay(_ attribute:NSLayoutAttribute,_ relatedBy:NSLayoutRelation,_ toItem:Any?,_ attribute1:NSLayoutAttribute , multiplier: CGFloat , constant: CGFloat)->NSLayoutConstraint{
+        self.translatesAutoresizingMaskIntoConstraints = false
+        let layout = NSLayoutConstraint(item: self, attribute: attribute, relatedBy: relatedBy, toItem: toItem, attribute: attribute1, multiplier: multiplier, constant: constant)
+        layout.isActive = true
+        return layout
+    }
+
+    @discardableResult func mLay(_ attribute:NSLayoutAttribute,_ relatedBy:NSLayoutRelation,_ toItem:Any?)->NSLayoutConstraint{
         return mLay(attribute, relatedBy, toItem, attribute, multiplier:1, constant:0)
     }
-    
-    func mLay(_ attribute:NSLayoutAttribute,_ constant: CGFloat)->NSLayoutConstraint{
+
+    @discardableResult func mLay(_ attribute:NSLayoutAttribute,_ constant: CGFloat)->NSLayoutConstraint{
         return mLay(attribute, .equal, nil, attribute,multiplier: 1, constant:constant)
     }
-    
-    func mLay(_ attribute:NSLayoutAttribute,_ relatedBy:NSLayoutRelation,_ toItem:Any?, multiplier: CGFloat, constant: CGFloat)->NSLayoutConstraint{
+
+    @discardableResult func mLay(_ attribute:NSLayoutAttribute,_ relatedBy:NSLayoutRelation,_ toItem:Any?, multiplier: CGFloat, constant: CGFloat)->NSLayoutConstraint{
         return mLay(attribute, relatedBy , toItem, attribute, multiplier:multiplier, constant:constant)
     }
-    
-    func mLay(_ attribute:NSLayoutAttribute,_ relatedBy:NSLayoutRelation,_ toItem:Any?,  constant: CGFloat)->NSLayoutConstraint{
+
+    @discardableResult func mLay(_ attribute:NSLayoutAttribute,_ relatedBy:NSLayoutRelation,_ toItem:Any?,  constant: CGFloat)->NSLayoutConstraint{
         return mLay(attribute, relatedBy , toItem, attribute, multiplier:1, constant:constant)
     }
-    
-    func mLay(_ attribute:NSLayoutAttribute,_ relatedBy:NSLayoutRelation,_ toItem:Any?,_ attribute1:NSLayoutAttribute ,constant: CGFloat)->NSLayoutConstraint{
+
+    @discardableResult func mLay(_ attribute:NSLayoutAttribute,_ relatedBy:NSLayoutRelation,_ toItem:Any?,_ attribute1:NSLayoutAttribute ,constant: CGFloat)->NSLayoutConstraint{
         return mLay(attribute, relatedBy , toItem, attribute1, multiplier:1, constant:constant)
     }
-    
-    func mLay(pin:UIEdgeInsets,to view:UIView)->[NSLayoutConstraint]{
-        return [
-            self.mLay(.top    , .equal , view , constant:  pin.top    ),
-            self.mLay(.left   , .equal , view , constant:  pin.left   ),
-            self.mLay(.bottom , .equal , view , constant: -pin.bottom ),
-            self.mLay(.right  , .equal , view , constant: -pin.right  ),
-        ]
+
+    @discardableResult func mLay(pin:UIEdgeInsets,to view:UIView? = nil)->[NSLayoutConstraint]{
+        assert(view != nil || superview != nil, "can't add Constraint to nil , superview and parmater view is nil")
+        return [  mLay(.top    , .equal , view ?? superview! , constant:  pin.top    ),
+                  mLay(.left   , .equal , view ?? superview! , constant:  pin.left   ),
+                  mLay(.bottom , .equal , view ?? superview! , constant: -pin.bottom ),
+                  mLay(.right  , .equal , view ?? superview! , constant: -pin.right  )]
     }
+
+    @discardableResult func mLayEqualSuper()->[NSLayoutConstraint]{
+        return mLay(pin: .zero)
+    }
+
+    @discardableResult func mLay(pin:OptionalEdge,to view:UIView? = nil)->[NSLayoutConstraint]{
+        assert(view != nil || superview != nil, "can't add Constraint to nil , superview and parmater view is nil")
+        var arr : [NSLayoutConstraint] = []
+        if let top    = pin.top   {  arr.append( mLay(.top    , .equal , view ?? superview!, constant:  top    ) ) }
+        if let left   = pin.left  {  arr.append( mLay(.left   , .equal , view ?? superview!, constant:  left   ) ) }
+        if let bottom = pin.bottom{  arr.append( mLay(.bottom , .equal , view ?? superview!, constant: -bottom ) ) }
+        if let right  = pin.right {  arr.append( mLay(.right  , .equal , view ?? superview!, constant: -right  ) ) }
+        return arr
+    }
+
+    @discardableResult func mLayCenterXY(to view:UIView? = nil)->[NSLayoutConstraint]{
+        assert(view != nil || superview != nil, "can't add Constraint to nil , superview and parmater view is nil")
+        return [ mLay(.centerY, .equal, view ?? superview! ),
+                 mLay(.centerX, .equal, view ?? superview! )]
+    }
+
+    @discardableResult func mLay(size:CGSize)->[NSLayoutConstraint]{
+        return [ mLay(.height, size.height),
+                 mLay(.width , size.width)]
+    }
+}
+
+extension NSLayoutConstraint {
+
+    @discardableResult func priority(_ pri: UILayoutPriority)->NSLayoutConstraint
+    {
+        self.priority = pri
+        return self
+    }
+    
 }
 
--- README.md
+++ README.md
@@ -6,7 +6,7 @@
 
 以下簡述一下用法
 
-            self.view.addSubview(button)
+            view.addSubview(button)
 
             // 設定 button 寬度為 160
             button.mLay(.width  , 160 ),
@@ -19,6 +19,91 @@
             
             // 設定 button 的 top 對齊 view 的 centerY 並偏移 150
             button.mLay(.top, .equal, view, .centerY, constant: 150)
+
+
+// 2017-07-29
+    相容舊版，以前的寫法都可以不用修改，另外再添加了下面的寫法
+
+    對於 method 添加了 @discardableResult 的修飾 , 回傳值可接可不接
+
+    預設 isActive = true ， 不用再手工 addConstraint
+    
+    以前為這樣寫
+        view.addSubview(button)
+        view.addConstraints([
+            button.mLay(.height, 50)
+        ])
+    現在只要這樣寫
+    button.mLay(.height, 50)
+
+    ----
+
+    添加了幾種寫法
+
+    如果是要對齊某個 view 的 centerX and centerY 以前會這樣寫
+    button.mLay(.centerX, .equal, view)
+    button.mLay(.centerY, .equal, view)
+
+    現在可以這樣寫
+    button.mLayCenterXY(to:view)
+    
+    如果 view 是 button 的 superview 還可以變成這樣寫
+    button.mLayCenterXY()
+
+    -
+
+    要設定某個元件的寬高 以前會這樣寫
+    button.mLay(.height, 100)
+    button.mLay(.width, 150)
+    
+    現在可以這樣寫
+    button.mLay(size: CGSize(width: 150, height: 100))
+
+    -
+
+    之前會這樣寫 
+    button.mLay(pin: .zero, to: view)
+
+    如果 view 是 button 的 superview 還可以變成這樣寫
+    button.mLay(pin: .zero)
+
+    也可以這樣寫
+    button.mLayEqualSuper()
+
+    -
+
+    mLay(pin: 的參數型別是 UIEdgeInsets , 只能 top left bottom right 通通都設定
+    新的 mLay(pin: 的參數添加接受另一種型別 OptionalEdge , 可以只設定部分方向
+
+    public struct OptionalEdge {
+        public var top: CGFloat?
+        public var left: CGFloat?
+        public var bottom: CGFloat?
+        public var right: CGFloat?
+        public init(top: CGFloat? = nil, left: CGFloat? = nil, bottom: CGFloat? = nil, right: CGFloat? = nil){
+            self.top = top
+            self.left = left
+            self.bottom = bottom
+            self.right = right
+        }
+    }
+
+    例如像是這樣
+    button.mLay(pin: .init(top: 20, left: 20, right: 20),to:view) // 跟 上 左 右 各保持 20 的距離 , 一樣 to: 不填會對齊 superview
+
+    -
+    
+    另外加上可以直接設定 NSLayoutConstraint 的 priority
+    以前會這樣寫
+    let layout = button.mLay(.height, 150)
+    layout.priority = 999
+
+    現在可以這樣寫
+    button.mLay(.height, 150).priority(999)
+
+
+
+
 
 
 
