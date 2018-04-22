3965522ca032e58046a6f9082cc3aff3cd3f2c49
Nick Lin <hawhaw.ya@gmail.com>
2018-04-22 23:16:20 +0800

    add pinSafe method add constant default 0

--- mLayout/Classes/mLayout.swift
+++ mLayout/Classes/mLayout.swift
@@ -23,7 +23,15 @@ public extension UIView {
         }
     }
 
-    @discardableResult func mLay( _ attribute: NSLayoutAttribute, _ relatedBy: NSLayoutRelation, _ toItem: Any?, _ attribute1: NSLayoutAttribute, multiplier: CGFloat, constant: CGFloat, active: Bool = true, priority: UILayoutPriority = .init(1000)) -> NSLayoutConstraint {
+    @discardableResult
+    func mLay( _ attribute: NSLayoutAttribute,
+               _ relatedBy: NSLayoutRelation,
+               _ toItem: Any?,
+               _ attribute1: NSLayoutAttribute,
+               multiplier: CGFloat,
+               constant: CGFloat,
+               active: Bool = true,
+               priority: UILayoutPriority = .init(1000)) -> NSLayoutConstraint {
         self.translatesAutoresizingMaskIntoConstraints = false
         let layout =  NSLayoutConstraint(item: self, attribute: attribute, relatedBy: relatedBy, toItem: toItem, attribute: attribute1, multiplier: multiplier, constant: constant)
         layout.priority = priority
@@ -31,55 +39,91 @@ public extension UIView {
         return layout
     }
 
-    @discardableResult func mLay(_ attribute: NSLayoutAttribute, _ relatedBy: NSLayoutRelation, _ toItem: Any?, active: Bool = true, priority: UILayoutPriority = .init(1000)) -> NSLayoutConstraint {
+    @discardableResult
+    func mLay(_ attribute: NSLayoutAttribute,
+              _ relatedBy: NSLayoutRelation,
+              _ toItem: Any?,
+              active: Bool = true,
+              priority: UILayoutPriority = .init(1000)) -> NSLayoutConstraint {
         return mLay(attribute, relatedBy, toItem, attribute, multiplier: 1, constant: 0, active: active, priority: priority)
     }
 
-    @discardableResult func mLay(_ attribute: NSLayoutAttribute, _ constant: CGFloat, active: Bool = true, priority: UILayoutPriority = .init(1000)) -> NSLayoutConstraint {
+    @discardableResult
+    func mLay(_ attribute: NSLayoutAttribute,
+              _ constant: CGFloat,
+              active: Bool = true,
+              priority: UILayoutPriority = .init(1000)) -> NSLayoutConstraint {
         return mLay(attribute, .equal, nil, attribute, multiplier: 1, constant: constant, active: active, priority: priority)
     }
 
-    @discardableResult func mLay(_ attribute: NSLayoutAttribute, _ relatedBy: NSLayoutRelation, _ toItem: Any?, multiplier: CGFloat, constant: CGFloat, active: Bool = true, priority: UILayoutPriority = .init(1000)) -> NSLayoutConstraint {
+    @discardableResult
+    func mLay(_ attribute: NSLayoutAttribute,
+              _ relatedBy: NSLayoutRelation,
+              _ toItem: Any?,
+              multiplier: CGFloat,
+              constant: CGFloat = 0,
+              active: Bool = true,
+              priority: UILayoutPriority = .init(1000)) -> NSLayoutConstraint {
         return mLay(attribute, relatedBy, toItem, attribute, multiplier: multiplier, constant: constant, active: active, priority: priority)
     }
 
-    @discardableResult func mLay(_ attribute: NSLayoutAttribute, _ relatedBy: NSLayoutRelation, _ toItem: Any?, constant: CGFloat, active: Bool = true, priority: UILayoutPriority = .init(1000)) -> NSLayoutConstraint {
+    @discardableResult
+    func mLay(_ attribute: NSLayoutAttribute,
+              _ relatedBy: NSLayoutRelation,
+              _ toItem: Any?,
+              constant: CGFloat,
+              active: Bool = true,
+              priority: UILayoutPriority = .init(1000)) -> NSLayoutConstraint {
         return mLay(attribute, relatedBy, toItem, attribute, multiplier: 1, constant: constant, active: active, priority: priority)
     }
 
-    @discardableResult func mLay(_ attribute: NSLayoutAttribute, _ relatedBy: NSLayoutRelation, _ toItem: Any?, _ attribute1: NSLayoutAttribute, constant: CGFloat, active: Bool = true, priority: UILayoutPriority = .init(1000)) -> NSLayoutConstraint {
+    @discardableResult
+    func mLay(_ attribute: NSLayoutAttribute,
+              _ relatedBy: NSLayoutRelation,
+              _ toItem: Any?,
+              _ attribute1: NSLayoutAttribute,
+              constant: CGFloat = 0,
+              active: Bool = true,
+              priority: UILayoutPriority = .init(1000)) -> NSLayoutConstraint {
         return mLay(attribute, relatedBy, toItem, attribute1, multiplier: 1, constant: constant, active: active, priority: priority)
     }
 
-    @discardableResult func mLay(pin: UIEdgeInsets, to view: UIView? = nil) -> [NSLayoutConstraint] {
+    @discardableResult
+    func mLay(pin: UIEdgeInsets, to view: UIView? = nil) -> [NSLayoutConstraint] {
         assert(view != nil || superview != nil, "can't add Constraint to nil , superview and parmater view is nil")
-        return [  mLay(.top, .equal, view ?? superview!, constant: pin.top    ),
-                  mLay(.left, .equal, view ?? superview!, constant: pin.left ),
-                  mLay(.bottom, .equal, view ?? superview!, constant: -pin.bottom ),
-                  mLay(.right, .equal, view ?? superview!, constant: -pin.right  )]
+        let view = view ?? superview!
+        return [  mLay(.top   , .equal, view, constant: pin.top     ),
+                  mLay(.left  , .equal, view, constant: pin.left    ),
+                  mLay(.bottom, .equal, view, constant: -pin.bottom ),
+                  mLay(.right , .equal, view, constant: -pin.right  )]
     }
 
-    @discardableResult func mLayEqualSuper() -> [NSLayoutConstraint] {
+    @discardableResult
+    func mLayEqualSuper() -> [NSLayoutConstraint] {
         return mLay(pin: .zero)
     }
 
-    @discardableResult func mLay(pin: OptionalEdge, to view: UIView? = nil) -> [NSLayoutConstraint] {
+    @discardableResult
+    func mLay(pin: OptionalEdge, to view: UIView? = nil) -> [NSLayoutConstraint] {
         assert(view != nil || superview != nil, "can't add Constraint to nil , superview and parmater view is nil")
         var arr: [NSLayoutConstraint] = []
-        if let top    = pin.top {  arr.append( mLay(.top, .equal, view ?? superview!, constant: top    ) ) }
-        if let left   = pin.left {  arr.append( mLay(.left, .equal, view ?? superview!, constant: left   ) ) }
-        if let bottom = pin.bottom {  arr.append( mLay(.bottom, .equal, view ?? superview!, constant: -bottom ) ) }
-        if let right  = pin.right {  arr.append( mLay(.right, .equal, view ?? superview!, constant: -right  ) ) }
+        let view = view ?? superview!
+        if let top    = pin.top    { arr.append( mLay(.top   , .equal, view, constant: top     )) }
+        if let left   = pin.left   { arr.append( mLay(.left  , .equal, view, constant: left    )) }
+        if let bottom = pin.bottom { arr.append( mLay(.bottom, .equal, view, constant: -bottom )) }
+        if let right  = pin.right  { arr.append( mLay(.right , .equal, view, constant: -right  )) }
         return arr
     }
 
-    @discardableResult func mLayCenterXY(to view: UIView? = nil) -> [NSLayoutConstraint] {
+    @discardableResult
+    func mLayCenterXY(to view: UIView? = nil) -> [NSLayoutConstraint] {
         assert(view != nil || superview != nil, "can't add Constraint to nil , superview and parmater view is nil")
         return [ mLay(.centerY, .equal, view ?? superview! ),
                  mLay(.centerX, .equal, view ?? superview! )]
     }
 
-    @discardableResult func mLay(size: CGSize) -> [NSLayoutConstraint] {
+    @discardableResult
+    func mLay(size: CGSize) -> [NSLayoutConstraint] {
         return [ mLay( .height, size.height), mLay( .width, size.width)]
     }
 
@@ -103,7 +147,8 @@ public extension UIView {
     }
 
     // Auto Active
-    @discardableResult func mLayEqualSafeArea(with item: UIView, direction: mLayDirection, constant: CGFloat = 0, active: Bool = true) -> NSLayoutConstraint {
+    @discardableResult
+    func mLayEqualSafeArea(with item: UIView, direction: mLayDirection, constant: CGFloat = 0, active: Bool = true) -> NSLayoutConstraint {
         self.translatesAutoresizingMaskIntoConstraints = false
         if #available(iOS 11.0, *) {
             switch direction {
@@ -118,10 +163,22 @@ public extension UIView {
         }
     }
 
+    @discardableResult
+    func mLaySafe(pin: OptionalEdge, to view: UIView? = nil) -> [NSLayoutConstraint] {
+        assert(view != nil || superview != nil, "can't add Constraint to nil , superview and parmater view is nil")
+        var arr: [NSLayoutConstraint] = []
+        let view = view ?? superview!
+        if pin.top != nil    { arr.append( mLayEqualSafeArea(with: view, direction: .top   )) }
+        if pin.left != nil   { arr.append( mLayEqualSafeArea(with: view, direction: .left  )) }
+        if pin.bottom != nil { arr.append( mLayEqualSafeArea(with: view, direction: .bottom)) }
+        if pin.right != nil  { arr.append( mLayEqualSafeArea(with: view, direction: .right )) }
+        return arr
+    }
 }
 
 public extension NSLayoutConstraint {
-    @discardableResult func active(bool: Bool) -> NSLayoutConstraint {
+    @discardableResult
+    func active(bool: Bool) -> NSLayoutConstraint {
         self.isActive = bool
         return self
     }
