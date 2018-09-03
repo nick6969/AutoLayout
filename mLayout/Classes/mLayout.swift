8b51d6d511c2ff485c5d99d09ec75164f7edd8d9
Nick Lin <hawhaw.ya@gmail.com>
2018-09-03 23:48:55 +0800

    調整

--- mLayout/Classes/mLayout.swift
+++ mLayout/Classes/mLayout.swift
@@ -168,10 +168,10 @@ public extension UIView {
         assert(view != nil || superview != nil, "can't add Constraint to nil , superview and parmater view is nil")
         var arr: [NSLayoutConstraint] = []
         let view = view ?? superview!
-        if let value = pin.top    { arr.append( mLayEqualSafeArea(with: view, direction: .top   , constant: value)) }
-        if let value = pin.left   { arr.append( mLayEqualSafeArea(with: view, direction: .left  , constant: value)) }
-        if let value = pin.bottom { arr.append( mLayEqualSafeArea(with: view, direction: .bottom, constant: value)) }
-        if let value = pin.right  { arr.append( mLayEqualSafeArea(with: view, direction: .right , constant: value)) }
+        if let value = pin.top    { arr.append( mLayEqualSafeArea(with: view, direction: .top   , constant:  value)) }
+        if let value = pin.left   { arr.append( mLayEqualSafeArea(with: view, direction: .left  , constant:  value)) }
+        if let value = pin.bottom { arr.append( mLayEqualSafeArea(with: view, direction: .bottom, constant: -value)) }
+        if let value = pin.right  { arr.append( mLayEqualSafeArea(with: view, direction: .right , constant: -value)) }
         return arr
     }
 }
